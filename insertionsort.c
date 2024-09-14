ccd112f7578b4cee1cfc9a58b5bba35e7e7ffb5d
Meet Shah <warmessgaming@gmail.com>
2024-09-14 09:36:39 +0530

    ok

--- insertionsort.c
+++ insertionsort.c
@@ -45,12 +45,18 @@ void main(){
 
 void inssort(int arr[],int n){
     int i,key,j;
+    char b = '0';
     for(i = 1;i < n; i++){
         key = arr[i];
         j = i-1;
         while(arr[j] > key && j >= 0){
+            b = '1';
             arr[j+1] = arr[j];
             j--;
+        }
+        if(b == '0'){
+            printf("Returning\n");
+            return;
         }
         arr[j+1] = key;
     }
